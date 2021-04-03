d2f706664b71a2e96597a1fd2f140592c0c62589
Nazarii Boichyshyn <boichyshyn@gmail.com>
2021-04-04 01:30:12 +0300

    Reference types
    Reference types
    Both reference variables point to one place.
    If first var is assigned with new object - second stays with previous reference

---
+++ DOTNET/DOTNET/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace DOTNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //REFERENCE TYPES
+            StringBuilder first = new StringBuilder();
+            first.Append("hello");
+            StringBuilder second = first;
+            first.Append(" world");
+            Console.WriteLine(second); // Prints hello world
+
+            StringBuilder third = new StringBuilder();
+            third.Append("hello");
+            StringBuilder fourth = third;
+            third.Append(" world");
+            third = new StringBuilder("goodbye");
+            Console.WriteLine(third); // Prints goodbye
+            Console.WriteLine(fourth); // Still prints hello world
+        }
+    }
+}
